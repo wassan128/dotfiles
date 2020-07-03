67d1c8326bc57923df4dfb91fff110c27c282a65
wassan128 <wassan.taiko.master@gmail.com>
2020-07-03 23:41:42 +0900

    WIP Fix install sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-DOTPATH=~/dotfiles
+DOTPATH=~/ghq/github.com/wassan128/dotfiles
 
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
+
+    if [ "$f" = ".config" ]; then
+        ln -snfv "$DOTPATH/$f/fish/config.fish" "$HOME/.config/fish/config.fish"
+        continue
+    fi
 
     ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
