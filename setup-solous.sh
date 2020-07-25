434ec90016a17c093f6a0e3624646696e52f1420
Tatsuya Egawa <wassan.taiko.master@gmail.com>
2020-07-25 10:44:06 +0900

    Update setup-solus

--- setup-solous.sh
+++ setup-solous.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+sudo eopkg install -y fish
+
 sudo eopkg update-repo -f
 sudo eopkg up -d
 
 sudo eopkg it -c system.devel
-sudo eopkg install git
-sudo eopkg install arc-gtk-theme
+sudo eopkg install -y git
+sudo eopkg install -y arc-gtk-theme
 sudo eopkg install -y paper-gtk-theme paper-icon-theme
-sudo eopkg install vim
+sudo eopkg install -y vim
+
+sudo mkdir -p /usr/local
+
+# install chrome
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
+sudo eopkg it google-chrome-*.eopkg
+sudo rm google-chrome-*.eopkg
+
+# install golang
+export LOCAL_GOVERSION="1.14.6"
+curl -sSfLO https://golang.org/dl/go$LOCAL_GOVERSION.linux-amd64.tar.gz
+tar -C /usr/local -xzf go$LOCAL_GOVERSION.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
