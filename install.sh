f232f169a0d6f66c057836461c9be879b1e5e0a0
wassan128 <wassan.taiko.master@gmail.com>
2020-10-08 23:30:03 +0900

    Change dir/files structure for vim/neovim

--- install.sh
+++ install.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
-DOTPATH=~/ghq/github.com/wassan128/dotfiles
+exe_path="${0}"
+if [[ "$exe_path" != './install.sh' ]]; then
+    echo '[ERROR] install.shはdotfile直下で実行してください。'
+    exit 1
+fi
 
+current_path="$(pwd)"
+
+# init
 for f in .??*
 do
+    # skip list
     [ "$f" = ".git" ] && continue
+    [ "$f" = "vim" ] && continue
 
     if [ "$f" = ".config" ]; then
-        ln -snfv "$DOTPATH/$f/fish/config.fish" "$HOME/.config/fish/config.fish"
-        ln -snfv "$DOTPATH/$f/starship.toml" "$HOME/.config/starship.toml"
+        ln -snfv "$current_path/$f/fish/config.fish" "$HOME/.config/fish/config.fish"
+        ln -snfv "$current_path/$f/starship.toml" "$HOME/.config/starship.toml"
         continue
     fi
 
-    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+    ln -snfv "$current_path/$f" "$HOME"/"$f"
 done
+
+# setup vim/neovim rc
+## vim
+ln -snfv "$current_path/vim/vimrc" "$HOME/.vimrc"
+ln -snfv "$current_path/vim/vim" "$HOME/.vim"
+## neovim
+ln -snfv "$current_path/vim/nvimrc" "$HOME/.config/nvim/init.vim"
+ln -snfv "$current_path/vim/nvim" "$HOME/.nvim"
